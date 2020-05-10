ffe522633a21e3ed65adc4a4b2bcef184f0358e4
tina_tao <tina_tao@tina-taodeMacBook-Air.local>
2020-05-09 17:48:29 -0700

    修复详细页无法显示问题，media与nginx冲突问题

--- blog/views.py
+++ blog/views.py
@@ -50,8 +50,9 @@ def detail(request, user_id_str):
 
 # 查看大图
 def enjoy(request, media_id_str):
+    print("media_id_str : ", media_id_str)
     media = Media.objects.get(media_id_str=media_id_str)
-    return render(request, 'blog/enjoy.html', context={'media': media})
+    return render(request, 'blog/enjoy.html', context={'enjoy_content': media})
 
 
 # 归档列表
