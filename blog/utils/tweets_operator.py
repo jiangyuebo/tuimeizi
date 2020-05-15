9743b6536ef8e7d03354283e1b99848c0a7c9f78
jiangyuebo <jiangyuebo@gmail.com>
2020-05-15 15:39:31 -0700

    fix fetch bug

--- blog/utils/tweets_operator.py
+++ blog/utils/tweets_operator.py
@@ -143,10 +143,10 @@ def fetch_latest_tweets_from_poster(api, exsist_tweets, poster):
                     flag = True
                 else:
                     flag = False
-            except:
+            except Exception as e:
                 print('---> error poster:%s' % poster.user_screen_name)
+                print('e', e)
                 is_finished = 1
-                pass
 
         # save
         if new_tweets is not None and len(new_tweets) > 0:
